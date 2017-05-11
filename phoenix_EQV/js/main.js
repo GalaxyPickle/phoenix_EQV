49b12a1325e5f107c351592b93be7be4ecadfc50
Alex <alex@clancylang.com>
2017-05-10 18:09:35 -0700

    initial filepath setup with few demo assets and no code

---
+++ phoenix_EQV/js/main.js
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////////////
+//														//
+//					Team Augment BOW					//
+//	Calvin Walantus										//
+//	Vienna Chan											//
+//	Alex Lang											//
+//	Shuoen Li											//
+//	Joey Sandmeyer										//
+//														//
+//				----Phoenix Equinox V----				//
+//														//
+//						main.js							//
+//														//
+//////////////////////////////////////////////////////////
+
+
+// Joey, paste your code here...
