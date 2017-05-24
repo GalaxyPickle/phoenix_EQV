1df90f697d1e369ae069e2744af061a7cd9e29b2
pigrocket <joey.sandmeyer@gmail.com>
2017-05-24 00:03:21 -0700

    phoejay glide normalized; phoejay jump suppressed

--- phoenix_EQV/js/prefabs/Phoejay.js
+++ phoenix_EQV/js/prefabs/Phoejay.js
@@ -13,24 +13,25 @@ function Phoejay(game, key, frame, x, y, hitbox_size=100/3, birdWeight=2) {
 	this.body.friction = 0.03;
 	this.scale.x = 1;
 	this.scale.y = 1;
-	this.body.gravityScale = birdWeight;
 
 	// set CUSTOM properties
 	this.cursors = game.input.keyboard.createCursorKeys();
 
-	this.turnspeed = 0.6;
-	this.boost = 9999.0;
-	this.jetpack = 20;
-	this.jetpackmax = 20;
-	this.birdWeight = 2;
-	this.grounded = false;
-	this.dir = 0; //direction left = -1; direction right = 1;
-	this.jump = null;
-	this.run = false;
-	this.lasty = null;
-
-	// add this bad boy to the game!
-	game.add.existing(this);
+	this.turnspeed = 0.6;               //                 ,dPYb,         
+	this.boost = 9999.0;                //                 IP'`Yb         
+	this.jetpack = 20;                  //                 I8  8I                              gg 
+	this.jetpackmax = 20;               //                 I8  8'                              ""     
+	this.grounded = false;              //     gg,gggg,    I8 dPgg,     ,ggggg,    ,ggg,       gg    ,gggg,gg  gg     gg 
+	this.jumpswitch = false;            //     I8P"  "Yb   I8dP" "8I   dP"  "Y8gggi8" "8i      8I   dP"  "Y8I  I8     8I 
+	this.jump = null;                   //     I8'    ,8i  I8P    I8  i8'    ,8I  I8, ,8I     ,8I  i8'    ,8I  I8,   ,8I 
+	this.run = false;                   //    ,I8 _  ,d8' ,d8     I8,,d8,   ,d8'  `YbadP'   _,d8I ,d8,   ,d8b,,d8b, ,d8I 
+	this.birdWeight = 2;                //    PI8 YY88888P88P     `Y8P"Y8888P"   888P"Y888888P"888P"Y8888P"`Y8P""Y88P"888
+	this.glide = 2.8;                   //     I8                                            ,d8I'                  ,d8I'
+                                        //     I8                                          ,dP'8I                 ,dP'8I 
+	// add this bad boy to the game!           I8                                         ,8"  8I                ,8"  8I 
+	game.add.existing(this);            //     I8                                         I8   8I                I8   8I 
+                                        //     I8                                         `8, ,8I                `8, ,8I 
+                                        //     I8                                          `Y8P"                  `Y8P"  
 }
 
 Phoejay.prototype = Object.create(Phaser.Sprite.prototype);
@@ -38,69 +39,26 @@ Phoejay.prototype.constructor = Phoejay;
 
 Phoejay.prototype.create = function(Phoejay) {
 	this.anchor.setTo(.5,.5);
-	this.animations.add('idle', [0], 1, false);
-	this.animations.add('run', [1, 2], 12, true);
-	this.animations.add('jump1', [3], 1, false);
-	this.animations.add('jump2', [4], 1, false);
-	this.animations.add('glide', [5], 1, false);
-}
-Phoejay.prototype.update = function(Phoejay) {
-	//console.log(this.jetpack);
+	this.animations.add('idle', [0], 1,  false); //                       __     __
+	this.animations.add('run', [1, 2], 12,true); //                      /  \~~~/  |
+	this.animations.add('jump1', [3], 1, false); //                ,----(     ..    )
+	this.animations.add('jump2', [4], 1, false); //               /      \__     __/
+	this.animations.add('glide', [5], 1, false); //              /|         (\  |(
+}                                                //             ^ \   /___\  /\ |
+Phoejay.prototype.update = function(Phoejay) {   //                |__|   |__| -"
 
-	// this needs to be rewritten and simplified----/////////////
-
-	if (this.body.touching.down) this.grounded = true;
-	dir = this.cursors.right.isDown - this.cursors.left.isDown; //direction facing
-
-	if (this.grounded) { //player.body.touching.down
-		this.jetpack = this.jetpackmax;
-		this.jump = 0;
-		if (!dir) this.animations.play('idle');
-	}
-
-	//vertical movement
-	if (dir) {
-		this.scale.x = -dir;
-		if (this.grounded && !this.running) {
-			this.animations.play('run');
-			this.running = true;
-		}
-		else this.running = false;
-		if (dir < 0) {
-			this.body.moveLeft(30);
-		} else if (dir > 0) {
-			this.body.moveRight(30);
-		}
-	}
-
-	//horizontal movement
-	if ((this.grounded || this.body.touching.downleft || this.body.touching.downright) && this.cursors.up.isDown) { //first jump
-		this.body.moveUp(150);
-		this.jump = 1;
-	}
-
-	if (this.jump == 1 && this.cursors.up.isDown) { //first jump shorthop
-		if (this.jetpack > 0) {
-			this.body.moveUp(this.jetpack * 4);
-			this.jetpack -= 1;
-		}
-		this.animations.play('jump1');
-	}
-	if (this.jump == 1 && !this.cursors.up.isDown) { //reset
-		this.jump = 2; 
-	}
-	if ((this.jump == 0 || this.jump == 2) && !this.grounded && this.cursors.up.isDown) { //first jump post-jump
-		this.body.moveUp(this.birdWeight * 500);
-		this.jump = 3;
-	}
-	if (this.jump == 3) {
-		if (this.cursors.up.isDown) {
-			if (this.body.y > this.lasty) this.body.moveUp(this.birdWeight * 10.2);
-			this.animations.play('glide');
-		}
-		else this.animations.play('jump2');
-	}
-	this.lasty = this.body.y;
+	this.body.touching.down&&(this.grounded=!0);
+	var dir=this.cursors.right.isDown-this.cursors.left.isDown;                                 this.jumpswitch=!this.jumpswitch&&this.cursors.up.isDown,
+	this.body.gravityScale=this.birdWeight, /* (\(\      */  this.grounded&&(this.jetpack=this.jetpackmax,this.jump=0,dir||this.animations.play("idle")),
+	dir&&(this.scale.x=-dir,               /*  ( -.-)    */                                    this.grounded&&!this.running?(this.animations.play("run"),
+	this.running=!0):this.running=!1,     /*   O_(")(")  */                                 dir<0?this.body.moveLeft(30):dir>0&&this.body.moveRight(30)),
+	this.jump>2&&(this.cursors.up.isDown?(this.body.y>this.body.shape.oldpos.y&&(this.body.gravityScale=0,this.body.shape.oldpos.y=this.body.shape.pos.y,
+	this.body.moveDown(20*this.glide)),                                                    this.animations.play("glide")):this.animations.play("jump2")),
+	(this.grounded||this.body.touching.downleft||this.body.touching.downright)                    &&this.jumpswitch&&(this.body.moveUp(150),this.jump=1),
+	1==this.jump&&this.cursors.up.isDown&&(this.jetpack>0&&(this.body.moveUp(4*this.jetpack), /*    (\ __/)      */                     this.jetpack-=1),
+	this.animations.play("jump1")),1!=this.jump||this.cursors.up.isDown||(this.jump=2),       /*    (=`.`=)     */
+	0!=this.jump&&2!=this.jump||this.grounded||!this.cursors.up.isDown||                      /*    (") (")    */ (this.body.moveUp(500*this.birdWeight),
+	this.jump=3),this.jumpswitch=this.cursors.up.isDown;
 
 	////////////////////////////////////////////////////////////////
 }
