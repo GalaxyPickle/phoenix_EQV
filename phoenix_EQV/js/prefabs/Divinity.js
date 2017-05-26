5ddec8c80e8dcc38144170378696909f2b1a52c3
pigrocket <joey.sandmeyer@gmail.com>
2017-05-25 21:19:55 -0700

    divinity working classes

--- phoenix_EQV/js/prefabs/Divinity.js
+++ phoenix_EQV/js/prefabs/Divinity.js
@@ -1,9 +1,16 @@
 //armada edit
-class Bubbles extends Phaser.Sprite {
+class Divinity extends Phaser.Sprite {
 	
-	constructor(game, key_div, key_bar, positions) {
+	constructor(game, key_div, key_bar, playerbody) {//, positions
 		//for positions.length : x = positions[i][0], y = positions[i][1]
+		var x = Math.random() * game.world.width;
+		var y = Math.random() * game.world.height;
+		var frame = Math.floor(Math.random()*95);
+		super(game, x, y, key_div, frame);
 		
+		this.height = Math.random()*3 + 16;
+		this.width = Math.random()*3 + 8;
+		this.player = playerbody;
 	}
 	
 	create() {
@@ -17,9 +24,18 @@ class Bubbles extends Phaser.Sprite {
 	update() {
 		this.timer--;
 		
+		this.ydistance = this.player.x + 20 - this.x;
+		this.xdistance = this.player.y + 20 - this.y;
 		
-		stamina.height = 2;
-		stamina.width = game.width;
-		stamina.fixedToCamera = true;
+		if (Math.abs(this.xdistance) < 20 && Math.abs(this.ydistance) < 20) this.destroy();
+		if (Math.abs(this.xdistance) < 50 && Math.abs(this.ydistance) < 50 ) {
+			if (this.xdistance < 0) this.x += 1;
+			else this.x -= 1;
+			if (this.ydistance > 0) this.y += 1;
+			else this.y -= 1;
+		}
+		//stamina.height = 2;
+		//stamina.width = game.width;
+		//stamina.fixedToCamera = true;
 	}
 }
