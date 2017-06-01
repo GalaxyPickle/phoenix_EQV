84efc1fae26d10dc35c0b4e9ca282e503fa90d41
pigrocket <joey.sandmeyer@gmail.com>
2017-05-31 19:35:13 -0700

    dead animal and divinity tweening

--- phoenix_EQV/js/prefabs/DeadAnimal.js
+++ phoenix_EQV/js/prefabs/DeadAnimal.js
@@ -1,44 +1,74 @@
-
-function DeadAnimal(game, key, frame, x, y, divRequired) {
-     // call to Pahser.Sprite // new Sprite(game, x, y, key, frame)
-     Phaser.Sprite.call(this, game, x, y, key, frame, divRequired);
-
-     // set sprite properties
-     this.scale.x = 1;
-     this.scale.y = 1;
-
-     //custom properties
-     var alive = false;
-     //this is for the main function
-     var animalStatus = 0;
-     //0 means dead, 1 means in process of reviving, 2 means alive
-
-     // add this bad boy to the game!
-     game.add.existing(this);
-}
-
-DeadAnimal.prototype = Object.create(Phaser.Sprite.prototype);
-DeadAnimal.prototype.constructor = DeadAnimal;
-
-DeadAnimal.prototype.update = function(DeadAnimal) {
-	function collideWithPhoejay(){
-		//this is for when the Phoejay collides with the dead animal
-		if  (animalStatus = 0){
-			alive = 1;
-			//so now in a state where animal is in the process of reviving
-			if (divCounter == divRequired)
-			{
-				//divCounter should be the global variable in Phoejay
-				animalStatus = 2;
-				alive = true;
-				divCounter = 0;
-			}
-			else if (divCounter != divRequired && timerRunsOut){
-				alive = 0;
-				//animal is dead again and the player can retouch  it again to retry
-				divCounter = 0;
-			}
-
+var divinity;
+class DeadAnimal extends Phaser.Sprite {
+	
+	constructor(game, key_animal, key_div, key_bar, playerbody, n, time) {
+		
+		var x = 100;
+		var y = 100;
+		super(game, x, y, key_animal);
+		
+		this.n = n;
+		this.time = time;
+		this.t = -100;
+		this.player = playerbody;
+		this.divinities = new Array();
 	}
-
+	
+	update() {
+		if (this.t >= 0) this.t--;
+		
+		if (this.t < 0 && this.t > -100) {
+			for (var i = 0; i < this.n; i++)
+				this.divinities[i].destroy();
+		}
+		
+		if (divinity >= this.n) this.success();
+		
+		this.ydistance = this.player.x + 20 - this.x + 10;
+		this.xdistance = this.player.y + 20 - this.y + 10;
+		this.distance = Math.sqrt(this.xdistance*this.xdistance + this.ydistance*this.ydistance);
+		
+		if (this.distance < 30 && this.t <= 0) this.spawnDivinity();
+	}
+		
+	spawnDivinity() {
+		for (var i = 0; i < this.n; i++) {
+			divinity = 0;
+			this.t = this.time;
+			this.divinities[i] = new Divinity(game, this.x, this.y, 'divinity', '', this.player, i);
+			game.add.existing(this.divinities[i]);
+		}
+	}
+	
+	success() {
+		divinity = 0;
+		console.log("congrats");
+		
+		game.add.sprite(300, 600, 'mushroom');
+	}
 }
+	/*
+DeadAnimal.prototype.update = function(DeadAnimal) {
+    
+    
+    function collideWithPhoejay(){
+        //this is for when the Phoejay collides with the dead animal
+        if  (animalStatus = 0){
+            alive = 1;
+            //so now in a state where animal is in the process of reviving
+            if (divCounter == divRequired)
+            {
+                //divCounter should be the global variable in Phoejay
+                animalStatus = 2;
+                alive = true;
+                divCounter = 0;
+            }
+            else if (divCounter != divRequired && timerRunsOut){
+                alive = 0;
+                //animal is dead again and the player can retouch  it again to retry
+                divCounter = 0;
+            }
+
+    }
+
+}*/
--- phoenix_EQV/js/prefabs/Divinity.js
+++ phoenix_EQV/js/prefabs/Divinity.js
@@ -1,38 +1,59 @@
-//armada edit
-
+var divinity;
 class Divinity extends Phaser.Sprite {
 	
-	constructor(game, key_div, key_bar, playerbody, index) {
+	constructor(game, x, y, key_div, key_bar, playerbody, index) {
 		
-		var x = playerbody.x + 350 + (index-4)*40;
-		var y = playerbody.x + 70;
 		var frame = Math.floor(Math.random()*1);
 		super(game, x, y, key_div);
 		
 		this.height = Math.random()*3 + 16;
 		this.width = Math.random()*3 + 8;
 		this.player = playerbody;
+		
+		this.xdest = playerbody.x + 350 + (index-4)*40; //x coordinate for divinity location
+		this.ydest = playerbody.x + 70; //y coordinate
+		
+		this.ready = false;
 	}
 	
 	update() {
 		
-		this.ydistance = this.player.x + 20 - this.x;
-		this.xdistance = this.player.y + 20 - this.y;
-		this.distance = Math.sqrt(this.xdistance*this.xdistance + this.ydistance*this.ydistance);
+		if (this.ready) {
 		
-		if (this.distance < 30) {
-			this.destroy();
-			this.player.divinity += 1;
-			console.log(this.player.divinity);
+			this.ydistance = this.player.x + 20 - this.x;
+			this.xdistance = this.player.y + 20 - this.y;
+			this.distance = Math.sqrt(this.xdistance*this.xdistance + this.ydistance*this.ydistance);
+			
+			if (this.distance < 30) {
+				divinity += 1;
+				this.destroy();
+			}
+			else if (this.distance < 170) {
+				if (this.player.x + 20 > this.x) this.x += 120/this.distance;
+				else this.x -= 120/this.distance;
+				if (this.player.y + 20 > this.y) this.y += 120/this.distance;
+				else this.y -= 120/this.distance;
+			}
+			//stamina.height = 2;
+			//stamina.width = game.width;
+			//stamina.fixedToCamera = true;
 		}
-		else if (this.distance < 170) {
-			if (this.player.x + 20 > this.x) this.x += 120/this.distance;
-			else this.x -= 120/this.distance;
-			if (this.player.y + 20 > this.y) this.y += 120/this.distance;
-			else this.y -= 120/this.distance;
+		
+		else {
+			this.ydistance = this.xdest - this.x;
+			this.xdistance = this.ydest - this.y;
+			this.distance = Math.sqrt(this.xdistance*this.xdistance + this.ydistance*this.ydistance);
+			
+			if (this.distance < 10) {
+				this.ready = true;
+				console.log("hi");
+			}
+			else {
+				if (this.xdest + 20 > this.x) this.x += .05*this.distance;
+				else this.x -= .5*this.distance;
+				if (this.ydest + 20 > this.y) this.y += .5*this.distance;
+				else this.y -= .5*this.distance;
+			}
 		}
-		//stamina.height = 2;
-		//stamina.width = game.width;
-		//stamina.fixedToCamera = true;
 	}
 }
