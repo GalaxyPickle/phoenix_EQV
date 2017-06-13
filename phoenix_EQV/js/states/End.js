295885461e33f2224652f52ec038a1b3a7d8bec6
Joeseph Thomas Sandmeyer <joey.sandmeyer@gmail.com>
2017-06-12 19:54:50 -0700

    beginning end state

--- phoenix_EQV/js/states/End.js
+++ phoenix_EQV/js/states/End.js
@@ -5,18 +5,81 @@
 
 var End = function(game) {
 	// variables
+	var story1 = null;
 };
 End.prototype = {
 	preload: function() {
 		console.log('End: preload');
 
-		game.stage.backgroundColor = "#333";
+		game.stage.backgroundColor = "#fff";
 
 
 	},
 	create: function() {
 		console.log('End: create')
+		
+		small_style = {fontSize: '20px', fill: '#aaa', font: 'Meta'};
+		
+				//timer to change first text
+		let timer01 = game.time.create(false);
+		let timedEvent01 = timer01.add(Phaser.Timer.SECOND * 7, updateText, game);
+		timer01.start();
+		//timer to change second text
+		let timer02 = game.time.create(false);
+		let timedEvent02 = timer01.add(Phaser.Timer.SECOND * 16, updateText2, game);
+		timer02.start();
+		//timer to change third text
+		let timer03 = game.time.create(false);
+		let timedEvent03 = timer01.add(Phaser.Timer.SECOND * 23, updateText3, game);
+		timer03.start();
+		//timer for the ray of light and revival fire
+		let timer04 = game.time.create(false);
+		timer03.start();
+		//time to go to game
+		let timer05 = game.time.create(false);
+		let timedEvent05 = timer01.add(Phaser.Timer.SECOND * 27, startGame, game);
+		timer03.start();
+
+		//add text for story
+		story1 = game.add.text(game.world.centerX, game.world.centerY, "After 100 years the phoenix Phoejay is finally reborn.", small_style);
+		story1.anchor.setTo(0.5, 0.5);
+		story1.bringToTop();
+		//make it fade in
+		story1.alpha = 1;
+		game.add.tween(story1).to( { alpha: 1 }, 2000, "Linear", true, 0);
+		
+		function updateText(){
+			//story1.alpha = 0;
+			//game.add.tween(story1).to( { alpha: 1 }, 2000, "Linear", true, 4000);
+			console.log("hi");
+			
+			story1.destroy();
+			story1 = game.add.text(game.world.centerX, game.world.centerY, "                   The forest is familiar yet foreign to the young phoenix. \nParts of the forest remain the same but it can tell that something has changed....", small_style);
+			story1.alpha = 0;
+			game.add.tween(story1).to( { alpha: 1 }, 2000, "Linear", true, 0);
+			story1.anchor.setTo(0.5, 0.5);
+		}
+			function updateText2(){
+			console.log("hi1");
+			
+			story1.destroy();
+			story1 = game.add.text(game.world.centerX, game.world.centerY, "The phoenix can feel something ominous in the air, but can't put its foot on what.", small_style);
+			story1.anchor.setTo(0.5, 0.5);
+			story1.alpha = 0;
+			game.add.tween(story1).to( { alpha: 1 }, 2000, "Linear", true, 0);
+		}
+		function updateText3(){
+			story1.destroy();
+			story1 = game.add.text(game.world.centerX, game.world.centerY, "And so it beings to explore...", small_style);
+			story1.anchor.setTo(0.5, 0.5);
+			story1.alpha = 0;
+			game.add.tween(story1).to( { alpha: 1 }, 2000, "Linear", true, 0);
+		}
+		function startGame(){
+			location.reload();
+		}
 	},
+		
 	update: function() {
 
 	}
